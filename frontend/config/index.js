6230c55fbe56b8c20b7026e3754ed96a1395194b
jaewon0913 <jaewon9937@gmail.com>
2022-06-04 16:26:47 +0900

    [기능추가] Vue 프로젝트 빌드시 Backend 폴더에 생성되도록 변경

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -38,12 +38,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../backend/src/main/resources/templates/vue/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsRoot: path.resolve(__dirname, '../../backend/src/main/resources/static/vue'),
+    assetsSubDirectory: 'static/',
+    assetsPublicPath: 'vue',
 
     /**
      * Source Maps
